a1ff32bc000e96e910cc8f5b118e2fc404da5c01
Monalize M <150396638+mlize08@users.noreply.github.com>
2025-03-17 00:24:15 +0000

    s

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     let tamanhoAtualFonte = 1;
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('aumentar-fonte');
 
     aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 })
